b0a5682341eb342a5dd57a892a58d2e25fac9407
prhasn <hasn@e.marnd.com>
2024-10-06 19:30:40 +0700

    coinexams package

---
+++ types.ts
@@ -0,0 +1,38 @@
+export type exchIds = `bin`;
+
+export interface portSettings {
+    /** 1 trade on | 0 trade off */
+    rb?: 1 | 0,
+
+    /** coins included list */
+    lst?: string[],
+
+    /** wallets holdings outside exchange, example { BTC: 0.01 } */
+    wal?: { [sy: string]: number },
+
+    /** manual distribution percentage, example for 50% { BTC: 50 } */
+    man?: { [sy: string]: number },
+
+    /** coinset Id */
+    coinSetId?: string,
+
+    /** exchange Id */
+    exchId?: exchIds,
+};
+
+export interface exchData {
+	/** holdings on exchange */
+	holdings: { [sy: string]: number },
+
+	/** positive numbers for buy, negative for sell */
+	trades?: { [sy: string]: number },
+
+	/** Time traded last ms */
+	lastTraded?: number,
+	
+	/** Next trade check ms */
+	nextCheck: number,
+	
+	/** exchange Id */
+   	exchId?: exchIds,
+}
